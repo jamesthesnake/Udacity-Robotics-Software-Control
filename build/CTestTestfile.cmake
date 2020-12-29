cb19403aa26341ce1e3fc22f6eaacd719a1c8186
James Hennessy <james.ryan.hennessy@gmail.com>
2020-12-28 23:45:09 -0800

    cmake files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/AURIS.LOCAL/james.hennessy/Downloads/catkin_ws/src
+# Build directory: /home/AURIS.LOCAL/james.hennessy/Downloads/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot")
+subdirs("simple_arm")
+subdirs("ball_chaser")
