bc59796250769a9d63c23c8b12b764c8494f67bc
jamesthesnake <james.ryan.hennessy@gmail.com>
2021-01-07 23:28:05 +0000

    dded catkin to project

---
+++ catkin_ws/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,19 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/catkin_ws/src
+# Build directory: /home/workspace/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(main)
+subdirs(turtlebot/turtlebot)
+subdirs(turtlebot/turtlebot_bringup)
+subdirs(turtlebot/turtlebot_capabilities)
+subdirs(turtlebot_simulator/turtlebot_gazebo)
+subdirs(turtlebot_simulator/turtlebot_simulator)
+subdirs(turtlebot_simulator/turtlebot_stage)
+subdirs(turtlebot_simulator/turtlebot_stdr)
+subdirs(odom_to_trajectory)
+subdirs(robot_pose_ekf)
+subdirs(turtlebot/turtlebot_teleop)
+subdirs(turtlebot/turtlebot_description)
