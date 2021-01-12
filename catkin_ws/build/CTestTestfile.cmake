1b26c5f870badcd0c3dc44b23c206d59d59580b2
jamesthesnake <james.ryan.hennessy@gmail.com>
2021-01-12 20:52:11 +0000

    add catskin

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/catkin_ws/src
+# Build directory: /home/workspace/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(main)
+subdirs(my_robot)
+subdirs(teleop_twist_keyboard-master)
+subdirs(ball_chaser)
---
+++ catkin_ws/devel/share/common-lisp/ros/ball_chaser/srv/_package_DriveToTarget.lisp
@@ -0,0 +1,10 @@
+(cl:in-package ball_chaser-srv)
+(cl:export '(LINEAR_X-VAL
+          LINEAR_X
+          ROUND_Y-VAL
+          ROUND_Y
+          ANGULAR_Z-VAL
+          ANGULAR_Z
+          MSG_FEEDBACK-VAL
+          MSG_FEEDBACK
+))
